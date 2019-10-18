e5603015116beae52236eb6f7b36f13ea3cdf6eb
Hussien Sleiman <hussien.sleiman@objectbrains.com>
2019-10-18 16:47:36 -0700

    more change

--- vacum.py
+++ vacum.py
@@ -3,6 +3,13 @@ import logging
 
 logger = logging.getLogger('vacum pump')
 file_log_handler = logging.FileHandler('/home/pi/log/vacum_pump_'+str(datetime.datetime.now())+'.log')
+stderr_log_handler = logging.StreamHandler()
+logger.addHandler(stderr_log_handler)
+
+# nice output format
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_log_handler.setFormatter(formatter)
+stderr_log_handler.setFormatter(formatter)
 
 var = 1
 while var == 1 :  # This constructs an infinite loop
@@ -13,10 +20,10 @@ while var == 1 :  # This constructs an infinite loop
 	GPIO.setwarnings(False)
 	GPIO.setup(18,GPIO.OUT)
         print "-----------------------------------------------"
-	print "Vacum on at " + str(datetime.datetime.now())
+	logger.info('Vacum on at ' + str(datetime.datetime.now()))
 	GPIO.output(18,GPIO.HIGH)
 	time.sleep(10)
-	print "Vacum off at " +  str(datetime.datetime.now())
+	logger.info('Vacum off at ' +  str(datetime.datetime.now()))
 	GPIO.output(18,GPIO.LOW)
 	time.sleep(300)
 	
