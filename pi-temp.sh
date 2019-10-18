d49becca4bc609e8471a5e1d1402c06d64c2f35f
Hussien Sleiman <hussien.sleiman@objectbrains.com>
2019-10-18 16:21:50 -0700

    changed the log out format.

--- pi-temp.sh
+++ pi-temp.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+LOG_DIR = /home/pi/log
 LOG_FILE = /home/pi/log/temp_$(date).log
-mkdir -p $LOG_FILE
+mkdir -p $LOG_DIR
+touch $LOG_FILE
 
 exec 3>&1 1>>$LOG_FILE 2>&1
 
 while true; do
 echo "-------------------------------------------"
 cpu=$(</sys/class/thermal/thermal_zone0/temp)
-echo "$(date)" | tee /dev/fd/3
-echo "GPU => $(/opt/vc/bin/vcgencmd measure_temp)" | tee /dev/fd/3
-echo "CPU => $(vcgencmd measure_clock arm)" | tee /dev/fd/3
-echo "CPU => $((cpu/1000))'C" | tee /dev/fd/3
+echo "$(date)" | tee -a $LOG_FILE
+echo "GPU => $(/opt/vc/bin/vcgencmd measure_temp)" | tee -a $LOG_FILE
+echo "CPU => $(vcgencmd measure_clock arm)" | tee -a $LOG_FILE
+echo "CPU => $((cpu/1000))'C" | tee -a $LOG_FILE
 sleep 10
 done
